02f22b9789a45fe7b3b3faf1a93c8541252b11b2
John Hawthorn <john.hawthorn@gmail.com>
2017-08-03 15:36:44 -0700

    Allow passing object to convert

--- lib/rabl_to_jbuilder.rb
+++ lib/rabl_to_jbuilder.rb
@@ -5,15 +5,17 @@ require 'ruby2ruby'
 require 'ruby_parser'
 
 module RablToJbuilder
-  def self.convert(rabl)
+  def self.convert(rabl, object: nil)
     parser    = RubyParser.new
     ruby2ruby = Ruby2Ruby.new
     root_node = parser.process(rabl)
 
     return "" unless root_node
 
-    match = root_node / Sexp.s(:call, nil, :object, Sexp._)
-    object = match[0][3] unless match.empty?
+    if !object
+      match = root_node / Sexp.s(:call, nil, :object, Sexp._)
+      object = match[0][3] unless match.empty?
+    end
 
     transformer = Transformer.new(object)
     converted = transformer.process(root_node)
